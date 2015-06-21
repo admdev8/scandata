b8a902f3ec92a736197528cc62c116c29adb256c
Jeff Thompson <mail@jeffreythompson.org>
2015-06-21 10:59:28 -0400

    Initial commit

--- index.php
+++ index.php
@@ -153,9 +153,9 @@
 		<div class="clear"></div>
 		<a name="info"></a>
 		<footer>
-			<p>These images were scraped from the <a href="https://archive.org">Internet Archive website</a>. Used for calibrating large book scanners, they automate the process of color correction and the removal of hot spots created by the scanner’s lights, and are meant to be read by the calibration software rather than humans.</p>
+			<p>The images in this collection were scraped from the <a href="https://archive.org">Internet Archive website</a>. Used for calibrating large book scanners, they automate the process of color correction and the removal of hot spots created by the scanner’s lights, and are meant to be read by the calibration software rather than humans.</p>
 
-			<p>When present in an Internet Archive entry, the images are stored in an odd file format (.ppm) inside a zip archive called “scandata.zip,” from which the title for this collection comes. Using a script written in Python, URLs for Internet Archive entries containing these files were pulled from the Bing search engine API, their contents downloaded, and the images converted to JPGs. They are shown here with a link to the Internet Archive entry from which they came. This collection represents a small fraction of the thousands of such files on Internet Archive’s site.</p>
+			<p>When present in an Internet Archive entry, the images are stored in an odd file format (.ppm) inside a zip archive called “scandata.zip,” from which the title for this collection comes. Using a <a href="https://github.com/jeffThompson/scandata">script written in Python</a>, URLs for Internet Archive entries containing these files were pulled from the Bing search engine API, their contents downloaded, and the images converted to JPGs. They are shown here with a link to the Internet Archive entry from which they came. This collection represents a small fraction of the thousands of such files on Internet Archive’s site.</p>
 
 			<p>All images copyright their creators, everything else <a href="http://creativecommons.org/licenses/by-nc-sa/3.0/">CC BY-NC-SA</a>.
 
